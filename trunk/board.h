cda4686222934c6607e45d76e713b934894da9b4
caronnee <caronnee@b328ddae-4ab3-11de-ba0e-b5da6ac7b600>
2009-05-30 08:14:48 +0000

    dobrna oprava v konstrukcii

--- trunk/board.h
+++ trunk/board.h
@@ -1,8 +1,8 @@
 #include <SDL/SDL.h>
+#include <vector>
 #include <iostream>
 #include <sstream>
 #include <math.h>
-
 
 #define BOARD_HEIGHT 430
 #define BOARD_WIDTH 430
@@ -13,35 +13,68 @@
 #define BOARD_Y_MAX 4
 
 extern SDL_Surface * screen;
+
+struct Triple
+{
+	int x, y, z;
+	Triple();
+	Triple(int x,int y,int z);
+	bool operator==(const Triple t);
+	bool operator==(const int i);
+	Triple& operator=(const int i);
+};
+
+class Figure;
+
 class Place
 {
 	//Jedno policko moze byt jednych hracom pokryte mac 16 figurakmi, tx 1<<4-1 je prvy hrac 1<<8-1&1<<4-1 druhy atd
 	int coord_x,coord_y; //suradnice stredu policka, kde sa to moze napasovat + kde sa bue vyznacovat krizik a pod.
-	int occupied_by; //ci na nom nejaka figurka stoji alebo niei a jej ID
+	Figure * occupied_by; //ci na nom nejaka figurka stoji alebo niei a jej ID
+	std::vector<Figure *> attackers;//pre kazde Place je zoznam toho, co ho ohrozuje, podla ID
 public:
-	bool attackers[16*3];//pre kazde Place je zoznam toho, co ho ohrozuje, stac ID
-	int under_attack; //kolko figurok utoci na dane policko
 	int get_x();
 	int get_y();
-	void occupy(int ID);
-	int occupied();
+	void occupy(Figure * ID);
+	Figure * occupied();
 	void set(int x, int y);//coordinates spocitane boardom
 	Place();
+};
+
+struct Gameboard
+{
+	Place board[PLAYERS][BOARD_X_MAX][BOARD_Y_MAX]; // pri prenasani sa bude menit prva suradnica podla toho, aky hrac to ma obdrzat
+	Place& operator[](const Triple t);
 };
 
 class Figure
 {
 protected:
-	bool active;
-	bool moved_;
-	int under_attack;
+	bool moved_; //figurka s pohla, dobre pre rosadu, pohyb pawna
+	bool under_attack; //ci je fugurka ohrozena, pomocne pri debugu
+	std::vector<Triple> legal_positions;
 	SDL_Surface * token;
 	SDL_Surface * display_token;
 	SDL_Surface * name;
 	SDL_Surface * choose;
 public:
-	int owner; //vlastnik figurky vzhadom na hraca pri pocitaci (1 aktualny hrac, 2 hrac druhy v poradi atd)
-	virtual int move() = 0; //Ktory pohyb ma vykonavat board
+
+	enum Tokens
+	{
+		KING, 
+		QUEEN, 
+		BISHOP, 
+		JUMPER, 
+		TOWER, 
+		PAWN,
+		NONE	
+	};
+
+	bool active; //ktora figurka to vlastne je
+	int owner; //vlastnik figurky vzhadom na hraca pri pocitaci (1 aktualny hrac, 2 hrac druhy v poradi atd, -1 = figurka je vyradena)
+	std::vector<Triple> moves();
+	virtual void check(Gameboard * g) = 0; //Kam vsade sa moze hybat
+	virtual std::vector<Triple> threats(); //co vsetko ohrozuje
 	void choosed(); // ak vybrana, zmeni sa jej vyzor..trosku
 	void unchoosed();
 	bool moved();
@@ -51,68 +84,62 @@ public:
 	Figure();
 };
 
+
 class King : public Figure
 {
 public:
-	King(int o);
-	virtual int move();
+	King( Triple t);
+	virtual void check(Gameboard * g);
 };
 
 class Queen : public Figure
 {
 public:
-	Queen(int o);
-	virtual int move();
+	Queen( Triple t);
+	virtual void check(Gameboard * g);
 };
 
 class Bishop : public Figure
 {
 public:
-	Bishop(int o);
-	virtual int move();
+	Bishop( Triple t);
+	virtual void check(Gameboard * g);
 };
 
 class Jumper : public Figure
 {
 public:
-	Jumper(int o);
-	virtual int move();
+	Jumper( Triple t);
+	virtual void check(Gameboard * g);
 };
 
 class Tower : public Figure
 {
 public:
-	Tower(int o);
-	virtual int move();
+	Tower( Triple t);
+	virtual void check(Gameboard * g);
 };
 
 class Pawn : public Figure
 {
-	int promotion; //ked sa changne, potom iba zmeni zposob, akym sa pohybuje a svoje oismeno
+	std::vector<Triple> threats_;
 public:
-	Pawn(int o);
-	virtual int move();
+	Pawn( Triple t);
+	virtual void check(Gameboard * g);
+	virtual std::vector<Triple> threats();
 };
 
 class Board
 {
 	int player_on_turn;
 	SDL_Surface * board_img;
-	int choosed_x, choosed_y, choosed_p;
-	Place board[PLAYERS][BOARD_X_MAX][BOARD_Y_MAX]; // pri prenasani sa bude menit prva suradnica podla toho, aky hrac to ma obdrzat
+	Triple choosed;
+	Gameboard board;
 	Figure* figures[48];
-	bool access[96];//vsetky policka, na ktore moze dana figurka, co je rpave na taku, skocit, budem potrebovat stale
 	int turn; //kto je prave na rade
-	int find_place(int x, int y); //vrati ID cislo figurky na policku so suradnicami x,y alebo -1, ak sa nenasla
-	void display_figure(int ID, int p, int x, int y);
-	void suggest_figure(int ID, int p, int x, int y);
-	void suggest_move_king(int p, int y, int x);
-	void suggest_move_queen(int p, int y, int x);
-	void suggest_move_bishop(int p, int y, int x);
-	void suggest_move_jumper(int p, int y, int x);
-	void suggest_move_tower(int p, int y, int x);
-	void suggest_move_pawn(int p,int y, int x); //samostany pohyb uz spravi chessboarda
-	void display_move(int player, int x, int y);
+	void display_figure(Triple t);
+	void suggest_figure(Figure *f, Triple t);
+	void display_move(Triple t);
 public:
 	Board();	
 	void draw_board(); //prejde cez vsetky figurky, co su na boarde a splitne ich na spravne miesto
