c24f997fd0db0da48528c59699add785b3866b96
caronnee <caronnee@b328ddae-4ab3-11de-ba0e-b5da6ac7b600>
2009-05-30 08:14:48 +0000

    dobrna oprava v konstrukcii

--- board.cpp
+++ board.cpp
@@ -4,56 +4,48 @@
 #define NUM_OF_PLACES 48
 #define RADIUS 200
 
-enum Tokens
-{
-	KING, 
-	QUEEN, 
-	BISHOP, 
-	JUMPER, 
-	TOWER, 
-	PAWN,
-	NONE	
-};
 
-int Place::occupied()
+Triple::Triple()
 {
-	return occupied_by;
+	x= y =z;
 }
-//x.y su aktualne policka v dvojrozmernom poli
-Place::Place()
+Triple::Triple(int x_,int y_,int z_)
 {
-	occupied_by = -1;
-	for ( int i =0; i < NUM_OF_PLACES; i++)
-		attackers[i] = false;
-	under_attack = 0;
-	coord_x = 0;
-	coord_y = 0; 
+	x = x_;
+	y = y_;
+	z = z_;
 }
-
-void Place::set(int x, int y)
+bool Triple::operator==(const Triple t)
 {
-	coord_x = x;
-	coord_y = y;
+	if ((t.x==x)&&(t.y == y)&&(t.z == z))
+		return true;
+	return false;
+}
+bool Triple::operator==(const int i)
+{
+	if ((x==i)||(y==i)||(z==i))
+		return true;
+	return false;
+}
+Triple& Triple::operator=(const int i)
+{
+	x=y=z=i;
+	return *this;
 }
 
-void Place::occupy(int ID)
+Place& Gameboard::operator[](const Triple t)
 {
-	occupied_by = ID;
+	return board[t.z][t.x][t.y];
 }
 
-int Place::get_x()
+std::vector<Triple> Figure::moves()
 {
-	return coord_x;
+	return legal_positions;
 }
 
-int Place::get_y()
+std::vector<Triple> Figure::threats()
 {
-	return coord_y;
-}
-
-int Figure::move()
-{
-	return -1;
+	return legal_positions;
 }
 
 void Figure::choosed()
@@ -72,8 +64,8 @@ bool Figure::moved()
 void Figure::remove()
 {
 	active = false;
-
 }
+
 SDL_Surface * Figure::display()
 {
 	return display_token;
@@ -87,145 +79,177 @@ SDL_Surface * Figure::display_name()
 Figure::Figure()
 {
 	moved_ = false;
+	under_attack = false;
 	owner = -1; //este nikto nemam zabookovanu figurku
 	active = true;
 	display_token = NULL;
 	choose = SDL_LoadBMP("./images/selected.bmp");
 	//SDL_SetColorKey(choose,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
-	under_attack = 0;
 }
 
-King::King(int o)
+Figure * Place::occupied()
 {
-	owner = o;
+	return occupied_by;
+}
+//x.y su aktualne policka v dvojrozmernom poli
+Place::Place()
+{
+	occupied_by = NULL;
+	coord_x = 0;
+	coord_y = 0; 
+}
+void Place::set(int x, int y)
+{
+	coord_x = x;
+	coord_y = y;
+}
+
+void Place::occupy(Figure * ID)
+{
+	occupied_by = ID;
+}
+
+int Place::get_x()
+{
+	return coord_x;
+}
+
+int Place::get_y()
+{
+	return coord_y;
+}
+
+King::King(Triple t)
+{
+	owner = t.z;
 	std::stringstream out;
-	out <<o;
+	out <<owner;
 	std::string s = "./images/token";
 	s+=out.str();
 	s+=".bmp";
 	token = SDL_LoadBMP(s.c_str());
 	name =  SDL_LoadBMP("./images/king.bmp");
-	if (name ==NULL) std::cerr << "huups!" <<std::endl;
-	if (token ==NULL) std::cerr << "huups2!" <<std::endl;
+	if (name == NULL) std::cerr << "King not found!" <<std::endl;
+	if (token ==NULL) std::cerr << "Token image not loaded!" <<std::endl;
 	//SDL_SetColorKey(token,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	//SDL_SetColorKey(name,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	display_token = token;
+	legal_positions.push_back(t);
 }
 
-int King::move()
+void King::check(Gameboard *g)
 {
-	return KING;
-}
-Queen::Queen(int o)
+}//TODO tu sa naplna, co kral moze ohrozit a kam sa pohnut
+Queen::Queen( Triple t)
 {	
-	owner = o;
+	owner = t.z;
 	std::stringstream out;
-	out <<o;
+	out <<owner;
 	std::string s = "./images/token";
 	s+=out.str();
 	s+=".bmp";
 	token = SDL_LoadBMP(s.c_str());
 	name =  SDL_LoadBMP("./images/queen.bmp");
-	if (name ==NULL) std::cerr << "huups!" <<std::endl;
-	if (token ==NULL) std::cerr << "huups2!" <<std::endl;
+	if (name ==NULL) std::cerr << "Queen image not found!" <<std::endl;
+	if (token ==NULL) std::cerr << "Token image not loaded!" <<std::endl;
 	//SDL_SetColorKey(token,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	//SDL_SetColorKey(name,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	display_token = token;
+	legal_positions.push_back(t);
 }
 
-int Queen::move()
+void Queen::check(Gameboard *g)
 {
-	return QUEEN;
-}
-Bishop::Bishop(int o)
+}//TODO tu sa naplna, co kral moze ohrozit a kam sa pohnut
+Bishop::Bishop( Triple t)
 {	
-	owner = o;
+	owner = t.z;
 	std::stringstream out;
-	out <<o;
+	out <<owner;
 	std::string s = "./images/token";
 	s+=out.str();
 	s+=".bmp";
 	token = SDL_LoadBMP(s.c_str());
 	name =  SDL_LoadBMP("./images/bishop.bmp");
-	if (name ==NULL) std::cerr << "huups!" <<std::endl;
-	if (token ==NULL) std::cerr << "huups2!" <<std::endl;
+	if (name ==NULL) std::cerr << "Bishop Image not found!" <<std::endl;
+	if (token ==NULL) std::cerr << "Token image not loaded!" <<std::endl;
 	//SDL_SetColorKey(token,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	//SDL_SetColorKey(name,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	display_token = token;
-
+	legal_positions.push_back(t);
 }
-
-int Bishop::move()
+void Bishop::check(Gameboard *g)
 {
-	return BISHOP;
-}
-Jumper::Jumper(int o)
+}//TODO tu sa naplna, co kral moze ohrozit a kam sa pohnut
+
+Jumper::Jumper( Triple t)
 {	
-	owner = o;
+	owner = t.z;
 	std::stringstream out;
-	out <<o;
+	out <<owner;
 	std::string s = "./images/token";
 	s+=out.str();
 	s+=".bmp";
 	token = SDL_LoadBMP(s.c_str());
 	name =  SDL_LoadBMP("./images/jumper.bmp");
-	if (name ==NULL) std::cerr << "huups!" <<std::endl;
-	if (token ==NULL) std::cerr << "huups2!" <<std::endl;
+	if (name ==NULL) std::cerr << "Jumper image not found!" <<std::endl;
+	if (token ==NULL) std::cerr << "Token image not loaded!" <<std::endl;
 	//SDL_SetColorKey(token,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	//SDL_SetColorKey(name,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	display_token = token;
+	legal_positions.push_back(t);
 }
 
-int Jumper::move()
+void Jumper::check(Gameboard *g)
 {
-	return JUMPER;
-}
-
-Tower::Tower(int o)
+}//TODO tu sa naplna, co kral moze ohrozit a kam sa pohnut
+Tower::Tower( Triple t)
 {	
-	owner = o;
+	owner = t.z;
 	std::stringstream out;
-	out <<o;
+	out << owner;
 	std::string s = "./images/token";
 	s+=out.str();
 	s+=".bmp";
 	token = SDL_LoadBMP(s.c_str());
 	name =  SDL_LoadBMP("./images/tower.bmp");
-	if (name ==NULL) std::cerr << "huups!" <<std::endl;
-	if (token ==NULL) std::cerr << "huups2!" <<std::endl;
+	if (name ==NULL) std::cerr << "Tower image not found!" <<std::endl;
+	if (token ==NULL) std::cerr << "Token image not loaded!" <<std::endl;
 	//SDL_SetColorKey(token,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	//SDL_SetColorKey(name,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	display_token = token;
+	legal_positions.push_back(t);
 }
 
-int Tower::move()
+void Tower::check(Gameboard *g)
 {
-	return TOWER;
-}
-
-Pawn::Pawn(int o)
+}//TODO tu sa naplna, co kral moze ohrozit a kam sa pohnut
+Pawn::Pawn( Triple t)
 {
-	owner = o;
+	owner = t.z;
 	std::stringstream out;
-	out <<o;
+	out <<owner;
 	std::string s = "./images/token";
 	s+=out.str();
 	s+=".bmp";
 	token = SDL_LoadBMP(s.c_str());
 	name =  SDL_LoadBMP("./images/pawn.bmp");
-	promotion = PAWN;
-	if (name ==NULL) std::cerr << "huups!" <<std::endl;
-	if (token ==NULL) std::cerr << "huups2!" <<std::endl;
+	if (name ==NULL) std::cerr << "Pawn image not found!" <<std::endl;
+	if (token ==NULL) std::cerr << "Token image not loaded!" <<std::endl;
 	//SDL_SetColorKey(token,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
 	//SDL_SetColorKey(name,SDL_SRCCOLORKEY, SDL_MapRGB(choose->format,125,125,125));
+	legal_positions.push_back(t);
 	display_token = token;
 }
 
-int Pawn::move()
+void Pawn::check(Gameboard *g)
 {
-	return promotion;
-}
+}//TODO tu sa naplna, co kral moze ohrozit a kam sa pohnut
 
+std::vector<Triple> Pawn::threats()
+{
+	return threats_;
+}//TODO tu sa naplna, co kral moze ohrozit a kam sa pohnut
 Board::Board()
 {
 	float size_x = RADIUS/8;//velkost nasho kroku
@@ -234,10 +258,8 @@ Board::Board()
 	int base = CENTER_Y + heigth;
 	coord_y_l = 1.83*RADIUS / 3;//vyska toho najposlednejsie (najviac vlavo)
 	board_img = SDL_LoadBMP("./images/chessboard.bmp");
-	choosed_x = -1; //este sme nic nevybrali
-	choosed_y = -1;
-	choosed_p = -1;
-	for (int x =0; x<4; x++)
+	choosed = -1;
+	for (int x = 0; x<4; x++)
 	{
 		coord_x = CENTER_X + (x-4)*size_x;
 		coord_y_l_last = coord_y_l;
@@ -258,9 +280,9 @@ Board::Board()
 			int rot_x_2 = rot_x + size_x*(7-2*x);
 			for (int player = 0; player < 3; player++)
 			{
-				board[player][x][y].set(rot_x * cos(2*3.14*(player)/3) - rot_y*sin(2*3.14*(player)/3)+ CENTER_X,
+				board.board[player][x][y].set(rot_x * cos(2*3.14*(player)/3) - rot_y*sin(2*3.14*(player)/3)+ CENTER_X,
 						rot_x * sin(2*3.14*(player)/3) + rot_y*cos(2*3.14*(player)/3)+ CENTER_Y);
-				board[player][7-x][y].set(rot_x_2 * cos(2*3.14*(player)/3) - rot_y*sin(2*3.14*(player)/3)+ CENTER_X,
+				board.board[player][7-x][y].set(rot_x_2 * cos(2*3.14*(player)/3) - rot_y*sin(2*3.14*(player)/3)+ CENTER_X,
 						rot_x_2 * sin(2*3.14*(player)/3) + rot_y*cos(2*3.14*(player)/3)+ CENTER_Y);
 
 			}
@@ -268,59 +290,59 @@ Board::Board()
 	}
 	for (int i = 0; i< 48; i+=16)
 	{
-		figures[i] = new Tower(i/16 );
-		figures[i+1] = new Jumper(i/16  );
-		figures[i+2] = new Bishop(i/16  );
-		figures[i+3] = new King(i/16  );
-		figures[i+4] = new Queen(i/16  );
-		figures[i+5] = new Bishop(i/16  );
-		figures[i+6] = new Jumper(i/16  );
-		figures[i+7] = new Tower(i/16  );
+		int owner = i/16;
+		figures[i] = new Tower(Triple(0,0,owner) );
+		figures[i+1] = new Jumper(Triple(1,0,owner) );
+		figures[i+2] = new Bishop(Triple(2,0,owner) );
+		figures[i+3] = new King(Triple(3,0,owner) );
+		figures[i+4] = new Queen(Triple(4,0,owner) );
+		figures[i+5] = new Bishop(Triple(5,0,owner) );
+		figures[i+6] = new Jumper(Triple(6,0,owner) );
+		figures[i+7] = new Tower(Triple(7,0,owner) );
 		for (int j = 0; j < 8; j++)
-			figures[i+8+j] = new Pawn(i/16 );
+			figures[i+8+j] = new Pawn( Triple(j,1,owner));
 	}//nacitane vsetky spravne so svojimi tokenmi
-	for ( int i = 0; i< 96 ; i++)
-		access[i] = false;
 	for (int i = 0; i < 3; i++)
 		for(int j = 0;j< 8; j++)
 			for (int k= 0; k< 2; k++)
 			{
-//				std::cout << "i,j,k "<< i << j <<k << "cislo figurky:" << 16*i + 8+k +j <<std::endl;
-				board[i][j][k].occupy(i*16 + k*8 + j ); //
+				board.board[i][j][k].occupy(figures[i*16 + k*8 + j] ); //
 			}
-	for (int i =0; i< NUM_OF_PLACES;i++) //vypocitaj pre vsetky figurky, ake policka pokryvaju
+	for (int i =0; i< 48;i++) //vypocitaj pre vsetky figurky, ake policka pokryvaju
 	{
+		figures[i]->check(&board); //nastavi aj figuram
 	}
 }
-void Board::suggest_figure(int ID, int p, int xx, int yy)
+void Board::suggest_figure(Figure *f,Triple t)
 {
 	SDL_Rect rect;
+	f->unchoosed();//potrebuje povodne cosi
 	rect.x =0; rect.y= 0;
-	figures[ID]->unchoosed();
-	rect.x = board[p][xx][yy].get_x()-TOKEN_WIDTH/2;
-	rect.y = board[p][xx][yy].get_y()-TOKEN_HEIGTH/2;
-	printf("%d [%d %d %d] %d %d\n",ID, p,xx,yy,board[p][xx][yy].get_x(),board[p][xx][yy].get_y());
-	SDL_SetAlpha(figures[ID]->display(), SDL_SRCALPHA, 100);
-	SDL_SetColorKey(figures[ID]->display(),SDL_SRCCOLORKEY, SDL_MapRGB(figures[ID]->display()->format,255,255,255));
-	SDL_BlitSurface(figures[ID]->display(),NULL,screen, &rect);
-	SDL_SetAlpha(figures[ID]->display_name(), SDL_SRCALPHA, 100);
-	SDL_SetColorKey(figures[ID]->display_name(),SDL_SRCCOLORKEY, SDL_MapRGB(figures[ID]->display_name()->format,255,255,255));
-	SDL_BlitSurface(figures[ID]->display_name(),NULL,screen, &rect);
-	figures[ID]->choosed();
+	rect.x = board[t].get_x()-TOKEN_WIDTH/2;
+	rect.y = board[t].get_y()-TOKEN_HEIGTH/2;
+//	printf("%d [%d %d %d] %d %d\n",ID, p,xx,yy,board[p][xx][yy].get_x(),board[p][xx][yy].get_y());
+	SDL_SetAlpha(f->display(), SDL_SRCALPHA, 100);
+	SDL_SetColorKey(f->display(),SDL_SRCCOLORKEY, SDL_MapRGB(f->display()->format,255,255,255));
+	SDL_BlitSurface(f->display(),NULL,screen, &rect);
+	SDL_SetAlpha(f->display_name(), SDL_SRCALPHA, 100);
+	SDL_SetColorKey(f->display_name(),SDL_SRCCOLORKEY, SDL_MapRGB(f->display_name()->format,255,255,255));
+	SDL_BlitSurface(f->display_name(),NULL,screen, &rect);
+	f->choosed();
 }
-void Board::display_figure(int ID, int p, int xx, int yy)
+void Board::display_figure(Triple t)
 {
 	SDL_Rect rect;
+	Figure * f = board[t].occupied();
 	rect.x =0; rect.y= 0;
-	rect.x = board[p][xx][yy].get_x()-TOKEN_WIDTH/2;
-	rect.y = board[p][xx][yy].get_y()-TOKEN_HEIGTH/2;
-	printf("%d [%d %d %d] %d %d\n",ID, p,xx,yy,board[p][xx][yy].get_x(),board[p][xx][yy].get_y());
-	SDL_SetAlpha(figures[ID]->display(), 0, 0);
-	SDL_SetColorKey(figures[ID]->display(),SDL_SRCCOLORKEY, SDL_MapRGB(figures[ID]->display()->format,255,255,255));
-	SDL_BlitSurface(figures[ID]->display(),NULL,screen, &rect);
-	SDL_SetAlpha(figures[ID]->display_name(), 0, 0);
-	SDL_SetColorKey(figures[ID]->display_name(),SDL_SRCCOLORKEY, SDL_MapRGB(figures[ID]->display_name()->format,255,255,255));
-	SDL_BlitSurface(figures[ID]->display_name(),NULL,screen, &rect);
+	rect.x = board[t].get_x()-TOKEN_WIDTH/2;
+	rect.y = board[t].get_y()-TOKEN_HEIGTH/2;
+//	printf("%d [%d %d %d] %d %d\n",ID, p,xx,yy,board[p][xx][yy].get_x(),board[p][xx][yy].get_y());
+	SDL_SetAlpha(f->display(), 0, 0);
+	SDL_SetColorKey(f->display(),SDL_SRCCOLORKEY, SDL_MapRGB(f->display()->format,255,255,255));
+	SDL_BlitSurface(f->display(),NULL,screen, &rect);
+	SDL_SetAlpha(f->display_name(), 0, 0);
+	SDL_SetColorKey(f->display_name(),SDL_SRCCOLORKEY, SDL_MapRGB(f->display_name()->format,255,255,255));
+	SDL_BlitSurface(f->display_name(),NULL,screen, &rect);
 }
 void Board::draw_board()
 {
@@ -331,21 +353,20 @@ void Board::draw_board()
 		for(int xx = 0; xx < 8; xx++) 
 			for(int y = 0; y< 4; y++)
 			{     
-				int ID = board[p][xx][y].occupied();
-				if ( ID >= 0 )
+				if ( board.board[p][xx][y].occupied()!=NULL )
 				{
 					//std::cout << ID <<std::endl;
-					display_figure(ID, p, xx,y);
+					display_figure(Triple(xx,y,p));
 				}
 			}
 	SDL_Flip(screen);
 }
 
-//TODO 
 bool Board::pick_up_figure(int x, int y)
 {
-//	printf("Suradnice %d %d\n", x,y);
-	int player, x_coord, y_coord, x_old, y_old, ID, choosed_ID = -1;
+	printf("Suradnice %d %d\n", x,y);
+	Triple new_choose;
+	int x_coord, y_coord, x_old, y_old;
 	x_coord = x;
 	y_coord = y;
 	bool found = false;
@@ -357,20 +378,20 @@ bool Board::pick_up_figure(int x, int y)
 		y_coord = (x_old -CENTER_X)*( sin(2*3.14/3)) + (y_old - CENTER_Y) * (cos(2*3.14/3)) + CENTER_Y; //Ako prvy bude player 3
 		for(int j = 0; j < BOARD_X_MAX; j++)
 		{
-			int diff_x = board[0][j][0].get_x() - x_coord; 
+			int diff_x = board.board[0][j][0].get_x() - x_coord; 
 			//printf("\tdiff : %d, [%d %d 0] %d %d \n",diff_x, i, j, board[i][j][0].get_x(), x_coord);
 			if ((diff_x <= TOKEN_WIDTH / 2)&&(diff_x>= -TOKEN_WIDTH/2)) //ak je rozdiel X dostatocne maly, potom sme kde na tejto strane	
 				for (int k = 0; k < BOARD_Y_MAX; k++)
 				{
-					int diff =board[0][j][k].get_y() - y_coord;  
+					int diff =board.board[0][j][k].get_y() - y_coord;  
 					if (( diff <= TOKEN_HEIGTH/2)&&(diff>= -TOKEN_HEIGTH/2))
 					{
 						found = true;
-						player = i;
-						x = j;
-						y = k;
-						ID = board[player][x][y].occupied();
-						if (choosed_p >-1) choosed_ID = board[choosed_p][choosed_x][choosed_y].occupied();
+						new_choose.z = i;
+						new_choose.x = j;
+						new_choose.y = k;
+						if (board[new_choose].occupied() == NULL) new_choose = choosed;
+						std::cout << "found!" << i <<" "<< j << " "<< k <<std::endl;
 					}
 					if (found) break;
 				}
@@ -378,76 +399,41 @@ bool Board::pick_up_figure(int x, int y)
 		}
 		if (found) break;
 	}
-	if (found)
+	if ((found)&&(new_choose.z !=-1))
 	{
-	/*	if ( (choosed_p > 0 ) && attack(board[choosed_p][choosed_x][choosed_y].occupied(),player,x,y) ) //je potrebne sa pohnut;)
-		{
-			remove(player,x,y);///whatever was on the place
-			remove(choosed_p,choosed_x, choosed_y);
-			(pla);
-		}*/
-		if ( ID  ==  choosed_ID)
+		if ( choosed  ==  new_choose)
 		{
 			printf("unchoosing");
-			figures[ID]->unchoosed();
-			//display_figure(board[choosed_p][choosed_x][choosed_y].occupied(), choosed_p, choosed_x, choosed_y);
-			draw_board();
-			choosed_x = choosed_y = choosed_p = -1;
+			board[choosed].occupied()->unchoosed();
+			choosed = -1;
 		}
 		else 
-		{	printf("%d %d %d %d",ID,player, x, y);
-			figures[ID]->choosed();
-			if(choosed_p !=-1)
+		{
+			std::cout << "here" <<std::endl;
+			board[new_choose].occupied()->choosed();
+			if(choosed.z !=-1)
 			{
-				figures[choosed_ID]->unchoosed();
-				display_figure(choosed_ID, choosed_p, choosed_x, choosed_y);
-				figures[board[choosed_p][choosed_x][choosed_y].occupied()]->unchoosed();
+				board[choosed].occupied()->unchoosed();
 			}
-			display_figure(ID , player, x, y);
-			display_move(player, x,y);
-			choosed_x = x;
-			choosed_y = y;
-			choosed_p = player;
+			display_figure(new_choose);
+		//	display_move(player, x,y);
+			choosed = new_choose;
 		}
+		draw_board();
 	}
 	SDL_Flip(screen);
 	return false;
 }
-void Board::display_move(int player_part, int x, int y)
+void Board::display_move(Triple pos)
 {
-	int ID = this->board[player_part][x][y].occupied();
-	int type = figures[ID]->move();
-	switch (type)
+	Figure * f = board[pos].occupied();
+	std::vector<Triple> move = f->moves();
+	for (unsigned int i =1; i< move.size(); i++) //na nultej pozicii ma ulozene svoje veci
 	{
-		case KING:
-			printf("King\n");
-			//suggest_move_king(int player_part, int x, int y);
-			break;
-		case QUEEN:
-			printf("Queen\n");
-			//suggest_move_queen(int player_part, int x, int y);
-			break;
-		case BISHOP:
-			printf("Bishop\n");
-			//suggest_move_bishop(int player_part, int x, int y);
-			break;
-		case JUMPER:
-			printf("Jumper\n");
-			//suggest_move_jumper(int player_part, int x, int y);
-			break;
-		case TOWER:
-			printf("Tower\n");
-			//suggest_move_tower(int player_part, int x, int y);
-			break;
-		case PAWN:
-			printf("Pawn\n");
-			suggest_move_pawn(player_part, x, y);
-			break;
-		default:
-			printf("Bola mi podhodena neznama figurka!\n");
+		suggest_figure(f, move[i]);
 	}
 }
-void Board::suggest_move_pawn(int player_part,int x, int y)
+/*void Board::suggest_move_pawn(int player_part,int x, int y)
 {
 	int ID = board[player_part][x][y].occupied();
 	std::cout <<"part:"<< player_part << ", owner" << figures[ID]->owner << std::endl;
@@ -471,4 +457,4 @@ void Board::suggest_move_pawn(int player_part,int x, int y)
 			player_part = (player_part + 2)%3;
 	}
 	suggest_figure(ID,player_part,x,y);
-}
+}*/
